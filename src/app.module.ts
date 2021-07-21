7c650c74e73a8c49842b183ee8503f7c09b0c1cf
Mateus Oliveira <mateus.mlo95@gmail.com>
2021-07-21 17:07:20 -0300

    add docker-compose and isolate sensitive data

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { dbConfig } from './config/db.config';
 import { VideosModule } from './videos/videos.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'root',
-      password: 'root',
-      database: 'aluraflix',
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [dbConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: 'localhost',
+        port: configService.get<number>('port'),
+        username: configService.get<string>('pgUser'),
+        password: configService.get<string>('pgPassword'),
+        database: configService.get<string>('db'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
     VideosModule,
   ],
