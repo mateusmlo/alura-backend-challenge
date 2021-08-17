4bd297f8f4e842bed362af1e6623581efa7e0141
Mateus Oliveira <mateus.mlo95@gmail.com>
2021-08-17 16:40:12 -0300

    renomeia containers e simplifica guards das rotas de categorias

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -11,7 +11,6 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
-import { Video } from 'src/videos/entities/video.entity';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { GetCategoriesFilterDto } from './dto/get-categories-filter.dto';
@@ -19,11 +18,11 @@ import { UpdateCategoryDto } from './dto/update-category.dto';
 import { Category } from './entities/category.entity';
 
 @Controller('categorias')
+@UseGuards(JwtAuthGuard)
 export class CategoriesController {
   private logger = new Logger('CategoriesController');
   constructor(private readonly categoriesService: CategoriesService) {}
 
-  @UseGuards(JwtAuthGuard)
   @Post()
   createCategory(
     @Body() createCategoryDto: CreateCategoryDto,
@@ -31,13 +30,11 @@ export class CategoriesController {
     return this.categoriesService.createCategory(createCategoryDto);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Get('/:id/videos')
   getCategoryVideos(@Param('id') id: string) {
     return this.categoriesService.getCategoryVideos(id);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Get()
   getCategories(
     @Query() filterDto: GetCategoriesFilterDto,
@@ -45,13 +42,11 @@ export class CategoriesController {
     return this.categoriesService.getCategories(filterDto);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Get('/:id')
   getCategoryById(@Param('id') id: string) {
     return this.categoriesService.getCategoryById(id);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Patch('/:id')
   updateCategory(
     @Param('id') id: string,
@@ -60,7 +55,6 @@ export class CategoriesController {
     return this.categoriesService.updateCategory(id, updateCategoryDto);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Delete('/:id')
   deleteCategory(@Param('id') id: string) {
     return this.categoriesService.deleteCategory(id);
